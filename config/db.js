ac04efb254e5f190507674c0d638b1805d1db268
Hamed Tabatabaeifar <hamedtabafar@gmail.com>
2025-08-28 16:50:57 +0330

    fixing db connection charset

--- config/db.js
+++ config/db.js
@@ -6,7 +6,9 @@ const dbPool = mysql.createPool({
     user: process.env.DB_USER || 'root', 
     password: process.env.DB_PASSWORD, 
     database: process.env.DB_NAME,
-    timezone: 'Z'
+    timezone: 'Z',
+    charset: 'utf8mb4'
+
 });
 
 module.exports = dbPool;
